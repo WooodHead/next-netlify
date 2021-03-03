e578961215e4f5ebd07fe485452086a61220e3f1
gty <gefyoung@gmail.com>
2021-03-03 15:25:07 -0600

    want to add notes to prod

--- pages/account/edit.js
+++ pages/account/edit.js
@@ -1,29 +1,30 @@
-import React, { useEffect, useRef, useState }  from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Amplify, { API, Auth } from 'aws-amplify'
 import '../../configureAmplify'
 import "../../node_modules/react-quill/dist/quill.snow.css"
 import NavbarComp from '../../components/navbar/navbar'
 import dynamic from 'next/dynamic'
 // import TopicString from '../../components/edit/topicString'
-const TopicString = dynamic(() => import('../../components/edit/topicString'),{ ssr: false })
+const TopicString = dynamic(() => import('../../components/edit/topicString'), { ssr: false })
 // const Topics = dynamic(() => import('../../components/edit/topics'),{ ssr: false })
-const ReactQuill = dynamic(() => import ('react-quill'),{ ssr: false })
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false })
 
 export default function Edit() {
 
-  const [userState, setUserState] = useState({ 
+  const [userState, setUserState] = useState({
     Username: 'loading...',
     ppm: 'loading...',
-    ratingAv: 'loading...' ,
+    ratingAv: 'loading...',
     publicString: 'loading...',
     topics: [],
   })
   const [publicStringState, setPublicStringState] = useState()
   const [editPubStringState, setEditPubStringState] = useState()
   const [publicQuillState, setPublicQuillState] = useState()
+  const [topicQuillState, setTopicQuillState] = useState()
   const [savedState, setSavedState] = useState()
-  const [topicState, setTopicState] = useState()
-  const [selectedTopicState, setSelectedTopic] = useState()
+  const [topicState, setTopicState] = useState([])
+  const [selectedTopicState, setSelectedTopic] = useState({topic: '', string: ''})
   const [newTopicState, setNewTopicState] = useState(false)
   const [editTopicState, setEditTopicState] = useState()
   const [savedTopicState, setSavedTopicState] = useState()
@@ -40,18 +41,20 @@ export default function Edit() {
   }
   const topicTypingFn = (e) => {
     setTopicQuillState(e)
-    setTopicSavedState(false)
+    setSavedTopicState(false)
   }
   const onCloseTopicEdit = () => {
-    setTopicEditState(false)
-    setTopicSavedState(false)
+    setEditTopicState(false)
+    setSavedTopicState(false)
   }
 
   const getUserData = async () => {
     const userSession = await Auth.currentAuthenticatedUser()
-    const getUserInit = { headers: { 
-      Authorization: userSession.attributes.preferred_username 
-    } }
+    const getUserInit = {
+      headers: {
+        Authorization: userSession.attributes.preferred_username
+      }
+    }
     try {
       const getAllUsersRes = await API.get(process.env.apiGateway.NAME, "/users", getUserInit)
       const topicsArray = []
@@ -90,25 +93,30 @@ export default function Edit() {
           string: `` + publicQuillState
         }
       }
-      const savedString = await API.post(process.env.apiGateway.NAME, '/saveStrings', stringInit )
+      const savedString = await API.post(process.env.apiGateway.NAME, '/saveStrings', stringInit)
       setSavedState(true)
-      setUserState({...userState}, { publicString: savedString.body })
+      setUserState({ ...userState }, { publicString: savedString.body })
     } catch (err) {
       console.log(err)
     }
   }
+
   const saveTopicString = async () => {
+    console.log(selectedTopicState.topic, topicQuillState)
     try {
       const userSession = await Auth.currentSession()
       const stringInit = {
         headers: { Authorization: userSession.idToken.jwtToken },
         body: {
           new: false,
-          string: `` + quillState
+          deleteTopic: false,
+          topic: selectedTopicState.topic,
+          string: `` + topicQuillState
         }
       }
-      const savedString = await API.post(process.env.apiGateway.NAME, '/saveStrings', stringInit )
+      const savedString = await API.post(process.env.apiGateway.NAME, '/topics', stringInit)
       setSavedTopicState(true)
+      console.log(savedString)
       // setStringState(savedString.body)
     } catch (err) {
       console.log(err)
@@ -126,11 +134,11 @@ export default function Edit() {
         string: null
       }
     }
-    const newTopic = await API.post(process.env.apiGateway.NAME, '/topics', newTopicParams )
-    console.log(newTopic)
-    setTopicState([...topicState, newTopic.body])
+    const newTopicRes = await API.post(process.env.apiGateway.NAME, '/topics', newTopicParams)
+    console.log(newTopicRes)
+    setTopicState([...userState.topics, newTopicRes.body])
     setNewTopicState(true)
-    
+    setSelectedTopic(newTopicRes.body)
   }
 
   const deleteTopic = async (topicProp) => {
@@ -145,7 +153,7 @@ export default function Edit() {
         string: null
       }
     }
-    await API.post(process.env.apiGateway.NAME, '/topics', deleteTopicParams )
+    await API.post(process.env.apiGateway.NAME, '/topics', deleteTopicParams)
     setTopicState(topicState.filter((topicObj) => topicObj.topic !== topicProp))
   }
 
@@ -160,41 +168,42 @@ export default function Edit() {
 
   return (
     <>
-    <NavbarComp />
-    <div className="mx-5">
-    <div className="flex flex-row bg-gray-100 my-5">
-    <div className="flex flex-col mx-5 my-5">
-      <h3 className='mx-5 my-5'>{userState.Username}</h3>
-    </div>
+      <NavbarComp />
+      <div className="mx-5">
+        <div className="flex flex-row bg-gray-100 my-5">
+          <div className="flex flex-col mx-5 my-5">
+            <h3 className='mx-5 my-5'>{userState.Username}</h3>
+          </div>
 
-    {editPubStringState 
-      ? <div>
-          <ReactQuill value={publicQuillState} onChange={typingFn}/>
-          <button onClick={onCloseEdit} >close</button>
-          <button onClick={savePublicString} >save</button>
-          {savedState && <div className="">
-            <div>
-              saved
+          {editPubStringState
+            ? <div>
+              <ReactQuill value={publicQuillState} onChange={typingFn} />
+              <button onClick={onCloseEdit} >close</button>
+              <button onClick={savePublicString} >save</button>
+              {savedState && <div className="">
+                <div>
+                  saved
             </div>
-        </div>}
-        </div>
-      : <div className="my-3" >
-          <div>
-          <div className="mx-3 my-3" dangerouslySetInnerHTML={{ __html: publicStringState }} ></div>
-          </div>
-          <div>
-            <button 
-              className="border-2 hover:border-black" 
-              onClick={() => setEditPubStringState(true)}
-            >
-              edit
+              </div>}
+            </div>
+            : <div className="my-3" >
+              <div>
+                <div className="mx-3 my-3" dangerouslySetInnerHTML={{ __html: publicStringState }} ></div>
+              </div>
+              <div>
+                <button
+                  className="border-2 hover:border-black"
+                  onClick={() => setEditPubStringState(true)}
+                >
+                  edit
             </button>
-          </div>
-        </div>
-      }
+              </div>
+            </div>
+          }
+
         </div>
         <div className="bg-gray-100" >
-          {userState.topics.map((topicObj) =>
+          {topicState.map((topicObj) =>
             <div key={topicObj.topic} >
               <button onClick={() => selectTopic(topicObj)} href={"/" + userState.Username + '/topic'}>
                 <a className="border-2 hover:border-black">{topicObj.topic}</a>
@@ -204,43 +213,37 @@ export default function Edit() {
           <input ref={topicInputRef}  ></input>
           <button onClick={createNewTopic}>create new topic</button>
         </div>
-        {/* <TopicString
-            user={userState}
-            topicState={selectedTopicState?.topic}
-            deleteTopic={deleteTopic}
-            stringState={selectedTopicState?.string}
-            editProps={newTopicState}
-          /> */}
-              <div className="my-5 bg-gray-100">
-      <div>{selectedTopicState?.topic}</div>
-    {editTopicState || newTopicState
-      ? <div>
-        <ReactQuill value={quillState} onChange={topicTypingFn} />
-        <button onClick={onCloseTopicEdit} >close</button>
-        <div className="border-2 my-3 mx-3 hover:border-black">
-            <button onClick={() => saveString(true)}>
-              save
+        <div className="my-5 bg-gray-100">
+          <div>{selectedTopicState?.topic}</div>
+
+          {editTopicState || newTopicState
+            ? <div>
+              <ReactQuill value={topicQuillState} onChange={topicTypingFn} />
+              <button onClick={onCloseTopicEdit} >close</button>
+              <div className="border-2 my-3 mx-3 hover:border-black">
+                <button onClick={() => saveTopicString(true)}>
+                  save
             </button>
-        </div>
-        {savedState && <div className="">
-            <div>
-              saved
+              </div>
+              {savedTopicState && <div className="">
+                <div>
+                  saved
             </div>
-        </div>}
+              </div>}
+            </div>
+            : <div>
+              <div className="mx-3 my-3" dangerouslySetInnerHTML={{ __html: selectedTopicState?.string }} ></div>
+              <button onClick={() => setEditTopicState(true)}>
+                <div className="border-2 my-3 mx-3 hover:border-black">edit</div>
+              </button>
+              <button onClick={() => deleteTopic(selectedTopicState?.topic)}>
+                <div className="border-2 my-3 mx-3 hover:border-black">delete</div>
+              </button>
+            </div>
+          }
+
+        </div>
       </div>
-      : <div>
-          
-          <div className="mx-3 my-3" dangerouslySetInnerHTML={{ __html: selectedTopicState?.string }} ></div>
-          <button onClick={() => setEditState(true)}>
-            <div className="border-2 my-3 mx-3 hover:border-black">edit</div>
-          </button>
-          <button onClick={() => deleteTopic(selectedTopicState?.topic)}>
-        <div className="border-2 my-3 mx-3 hover:border-black">delete</div>
-      </button>
-      </div>
-    }
-  </div>
-    </div>
     </>
   )
 }
