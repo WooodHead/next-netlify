f7804b510ffedd0686e9e74d4b9910e3ae08770a
gty <gefyoung@gmail.com>
2021-03-28 18:37:08 -0500

    reAdded withPWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const { PHASE_PRODUCTION_BUILD } = require('next/constants')
-// const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')
 
 module.exports = (phase) => {
 
@@ -29,5 +29,5 @@ module.exports = (phase) => {
     }
   }
 
-  return { env }
+  return withPWA({ env })
 }
