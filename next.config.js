bd3105ed078a0233f84ea11a31bca1bef9c217a2
gty <gefyoung@gmail.com>
2021-06-19 21:15:12 -0600

    changed cognito prod config

--- next.config.js
+++ next.config.js
@@ -24,15 +24,18 @@ module.exports = (phase) => {
     },
     cognito: {
       REGION: "us-east-1",
-      USER_POOL_ID: isProd ? "us-east-1_lfnYkHZ0h"
+      USER_POOL_ID: isProd ? "us-east-1_b1l8O5a6e"
         : "us-east-1_E1rXwEwFG",
+        // old prod "us-east-1_lfnYkHZ0h"
         // old dev "us-east-1_tQ7eQDxrO"
-      APP_CLIENT_ID: isProd ? "15bvc7hrss54rp3rodve8ncnao"
+      APP_CLIENT_ID: isProd ? "6fqtglce77tt8i60tmsm83e7kp"
         : "4u5e0sjuv242pf7os0pho22rae",
         // old dev "13tccrpqrs740rua48kbl8kt70"
-      IDENTITY: isProd ? "us-east-1:914c9af9-eac4-4e24-ad2f-6d9a341be684"
+        //old prod "15bvc7hrss54rp3rodve8ncnao"
+      IDENTITY: isProd ? "us-east-1:647dbf69-9432-4411-a5d4-78ba23dab8f9"
         : "us-east-1:6a389b4a-87a3-4738-bfb1-878fe5105f5d" ,
         //old dev "us-east-1:fc28ba4c-cf45-4a2e-bab7-a6bff48b857b"
+        // old prod "us-east-1:914c9af9-eac4-4e24-ad2f-6d9a341be684"
     },
     storage: {
       BUCKET: isProd ? 'tt3-s3-prod-imagesbucket-80gqbqnc042a' : 'tt3-s3-dev-imagesbucket-ohmvfcukxwiv',
