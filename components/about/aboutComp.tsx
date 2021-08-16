81f253357ce59fb7c137e196da63d3693ab1bfad
gty <gefyoung@gmail.com>
2021-08-16 14:21:47 -0600

    cleaned up tos, should have no 404 pages active now

--- components/about/aboutComp.tsx
+++ components/about/aboutComp.tsx
@@ -6,31 +6,38 @@ export default function AboutComp() {
   const router = useRouter()
   return (
     <>
-    <NavbarComp />
+      <NavbarComp />
       <div className="m-10">
         <div className="max-w-prose">
-        Talktree is a platform for on demand communication. 
-        Users can make and receive messages through the web app.
-        Text, audio, video, and screenshare.
-        Create an account and charge a price as low as $0.17/per minute to talk to you.
+          Talktree is a platform for on demand communication.
+          Users can make and receive messages through the web app.
+          Text, audio, video, and screenshare.
+          Create an account and charge a price as low as $0.17/per minute to talk to you.
         </div>
         <div className="mt-5">This website is in beta</div>
         <div>Email geoff@Talktree.me or <a href="/geoff-young" >Talktree.me/geoff-young</a></div>
         <div className="mt-5">
-          
-          
-          <button onClick={() => router.push("tos")}className="px-2 py-1 mx-5 my-1 rounded hover:bg-gray-200 ">
-          <Link href="tos"><a>Terms of Service</a></Link>
-            </button>
-          
-          <Link href="privacy">
-          <button className="px-2 py-1 mx-5 my-1 rounded hover:bg-gray-200 " >
-            Privacy Policy
-            </button>
+
+
+          <button
+            onClick={() => router.push("/about/tos")}
+            className="px-2 py-1 mx-5 my-1 rounded hover:bg-gray-200 "
+          >
+            <Link href="/about/tos">
+              <a>Terms of Service</a>
             </Link>
-          
+          </button>
+
+          <button
+            onClick={() => router.push("/about/privacy")}
+            className="px-2 py-1 mx-5 my-1 rounded hover:bg-gray-200 "
+          >
+            <Link href="/about/privacy">
+              <a>Privacy Policy</a>
+            </Link>
+          </button>
         </div>
       </div>
-      </>
+    </>
   )
 }
