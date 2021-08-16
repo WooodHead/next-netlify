13a98c77117f361db28e87a06d5e12fd231fd85f
gty <gefyoung@gmail.com>
2021-08-16 13:28:14 -0600

    cleaned up /users, was getting hella data in the html i think from getting all users?

--- pages/users.js
+++ pages/users.js
@@ -8,11 +8,6 @@ import Footer from '../components/navbar/footer'
 const Users = ({ allUsers }) => {
   const router = useRouter()
 
-  // const userClickFn = (userObjProp) => {
-  //   console.log(userObjProp.topics[0].title.S)
-  //   const titleIfExists = userObjProp.topics[0] ? userObjProp.topics[0].title.S : ''
-  //   // router.push(`/${userObjProp.Username + "/" + titleIfExists}`)
-  // }
   const clickUser = (usernameProp) => {
     router.push(usernameProp)
   }
@@ -31,30 +26,18 @@ const Users = ({ allUsers }) => {
                 <div className="mx-5 my-2">
                   <Link
                     className=""
-                    href={"/" + user.Username}
-                    // passHref
+                    href={`/${encodeURIComponent(user.Username)}`}
                   >
                     <a>{user.Username}</a>
                   </Link>
                   <div 
-                    
                     className="flex flex-row overflow-auto"
                   >
-                    
                     <div className="flex flex-col w-20">
                       <div>
                         {user.TAVS}
                       </div>
                     </div>
-
-                    {/* <div className="flex flex-col flex-wrap max-h-20">
-                      {user.topics.map((topicObj) =>
-                        <div className="mx-5" key={topicObj.topicId} >
-                          {topicObj.title.S.replace(/-/g, ' ')}
-                        </div>
-                      )
-                      }</div> */}
-
                     <div className="ml-2" dangerouslySetInnerHTML={{ __html: user.publicString }}>
                     </div>
                   </div>
@@ -75,12 +58,6 @@ export async function getStaticProps() {
   const getAllUsersRes = await API.get(process.env.NEXT_PUBLIC_APIGATEWAY_NAME, "/users", allUsersInit)
   getAllUsersRes.body.Items.forEach((userRes) => {
     const TAVS = []
-    const topicsArray = []
-    for (const [key, topicObj] of Object.entries(userRes.topics?.M)) {
-      if (!topicObj.M.draft.BOOL) {
-        topicsArray.push({ ...topicObj.M, topicId: key })
-      }
-    }
     userRes.deviceInput.M.text.BOOL && TAVS.push("📝")
     userRes.deviceInput.M.audio.BOOL && TAVS.push("📞")
     userRes.deviceInput.M.video.BOOL && TAVS.push("📹")
@@ -93,7 +70,6 @@ export async function getStaticProps() {
       ppm: userRes.ppm.N,
       ratingAv: userRes.ratingAv?.S || null,
       publicString: userRes.publicString?.S || null,
-      topics: topicsArray
     })
   })
   return { props: { allUsers: newAllUsers }, revalidate: 1 }
