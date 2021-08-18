abd538aaa1561a354ddcd7fb5de2b2523cf8716f
gty <gefyoung@gmail.com>
2021-08-18 17:04:25 -0600

    404 page kinda works now, ugly but gets username and most similar topic

--- pages/404.js
+++ pages/404.js
@@ -1,30 +1,48 @@
 import API from '@aws-amplify/api'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import '../configureAmplify'
+import NavbarComp from '../components/navbar/navbar'
+import stringSimilarity from 'string-similarity'
 
 export default function FourOhFour() {
+  const [state, setState] = useState({
+    username: '',
+    topics: '',
+    bestMatch: ''
+  })
   const router = useRouter()
-  console.log(router.asPath)
   
   useEffect(() =>{
     const getAssumedUser = async () => { 
       const idMaybe = router.asPath.match(/\/(.+?)\//)
+      const badTopic = router.asPath.match(/\/.+\/(.+)/)
 
       const specificUserInit = { headers: { Authorization: idMaybe[1] } }
       const getUserRes = await API.get(process.env.NEXT_PUBLIC_APIGATEWAY_NAME, "/users", specificUserInit)
-      console.log('getuserres', getUserRes)
+      const topicList = []
+      for (const [key, properties] of Object.entries(getUserRes.Item.topics.M)) {
+        if (!properties.M.draft.BOOL) {
+        topicList.push(properties.M.title.S)
+        }
+      }
+      const bestMatch = stringSimilarity.findBestMatch(badTopic[1], topicList)
+      console.log('bestmatch', bestMatch.bestMatch.target)
+      console.log('badtopic', badTopic[1])
+      setState({...state, 
+        username: getUserRes.Item.Username.S,
+        topics: topicList,
+        bestMatch: bestMatch.bestMatch.target
+      })
     }
+
     getAssumedUser()
   }, [])
   
   return <>
-    <h1>404 - Page Not Found</h1>
-    <Link href="/">
-      <a>
-        Go back home
-      </a>
-    </Link>
+    <NavbarComp />
+    <div className="flex justify-center text-2xl">404 - Page Not Found</div>
+    <div>Maybe you meant to visit talktree.me/{state.username}/{state.bestMatch}</div>
   </>
 }
