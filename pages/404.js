307b9dec69a118d2616993905b5dde66dfaae934
gty <gefyoung@gmail.com>
2021-08-18 16:05:25 -0600

    404 should work now

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,24 @@
 import API from '@aws-amplify/api'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import '../configureAmplify'
 
 export default function FourOhFour() {
   const router = useRouter()
   console.log(router.asPath)
   
-  const specificUserInit = { headers: { Authorization: params.id } }
-  const getUserRes = await API.get(process.env.NEXT_PUBLIC_APIGATEWAY_NAME, "/users", specificUserInit)
+  useEffect(() =>{
+    const getAssumedUser = async () => { 
+      const idMaybe = router.asPath.match(/\/(.+?)\//)
+
+      const specificUserInit = { headers: { Authorization: idMaybe[1] } }
+      const getUserRes = await API.get(process.env.NEXT_PUBLIC_APIGATEWAY_NAME, "/users", specificUserInit)
+      console.log('getuserres', getUserRes)
+    }
+    getAssumedUser()
+  }, [])
+  
   return <>
     <h1>404 - Page Not Found</h1>
     <Link href="/">
